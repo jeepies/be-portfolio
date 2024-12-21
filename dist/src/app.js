ead50c414e42dc2f1b9c075ba770803dcfd7cede
jay <jay@jeepies.codes>
2024-12-21 13:59:17 +0000

    feat: add routing

--- dist/src/app.js
+++ dist/src/app.js
@@ -4,6 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
+Object.entries(routes_1.default).forEach(([route, handlers]) => {
+    if (route === 'root')
+        route = '';
+    app.use(`/${route}`, handlers);
+});
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
+Object.entries(routes).forEach(([route, handlers]) => {
+    if (route === 'root') route = '';
+    app.use(`/${route}`, handlers);
+});
 
 export default app; 
