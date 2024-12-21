91c47076d249d90aa08e8af56e0f5cb964364a9b
jay <jay@jeepies.codes>
2024-12-21 13:20:33 +0000

    hf: add basic response

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express, { Express} from 'express';
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
+
+app.get('/', (_request: Request, response: Response) => {
+    response.send("hi :3");
+})
 
 export default app;
